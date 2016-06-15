f58a199e255fc709074046ff38514a5b6cb67f91
Facundo Scoccia <fanky10@gmail.com>
2016-06-15 10:04:45 -0300

    Fixed DATABASE_URL env name

--- src/main/java/ar/edu/utn/frro/config/HerokuDatabaseConfiguration.java
+++ src/main/java/ar/edu/utn/frro/config/HerokuDatabaseConfiguration.java
@@ -1,7 +1,10 @@
 package ar.edu.utn.frro.config;
 
+import java.util.Arrays;
+
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
@@ -14,28 +17,40 @@ import javax.sql.DataSource;
 @Profile(Constants.SPRING_PROFILE_HEROKU)
 public class HerokuDatabaseConfiguration {
 
-    private final Logger log = LoggerFactory.getLogger(HerokuDatabaseConfiguration.class);
+	private final Logger log = LoggerFactory
+			.getLogger(HerokuDatabaseConfiguration.class);
 
-    @Bean
-    public DataSource dataSource(DataSourceProperties dataSourceProperties, JHipsterProperties jHipsterProperties) {
-        log.debug("Configuring Heroku Datasource");
+	@Bean
+	public DataSource dataSource(DataSourceProperties dataSourceProperties,
+			JHipsterProperties jHipsterProperties) {
+		log.debug("Configuring Heroku Datasource");
 
-        String herokuUrl = System.getenv("JDBC_DATABASE_URL");
-        if (herokuUrl != null) {
-	    HikariConfig config = new HikariConfig();
+		String herokuUrl = System.getenv("DATABASE_URL");
+		if (herokuUrl != null) {
+			log.info("Web application on Heroku, using database url: {}", herokuUrl);
+			HikariConfig config = new HikariConfig();
 
-	    //MySQL optimizations, see https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
-	    if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(dataSourceProperties.getDriverClassName())) {
-                config.addDataSourceProperty("cachePrepStmts", jHipsterProperties.getDatasource().isCachePrepStmts());
-                config.addDataSourceProperty("prepStmtCacheSize", jHipsterProperties.getDatasource().getPrepStmtCacheSize());
-                config.addDataSourceProperty("prepStmtCacheSqlLimit", jHipsterProperties.getDatasource().getPrepStmtCacheSqlLimit());
-            }
+			// MySQL optimizations, see
+			// https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
+			if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource"
+					.equals(dataSourceProperties.getDriverClassName())) {
+				config.addDataSourceProperty("cachePrepStmts",
+						jHipsterProperties.getDatasource().isCachePrepStmts());
+				config.addDataSourceProperty("prepStmtCacheSize",
+						jHipsterProperties.getDatasource()
+								.getPrepStmtCacheSize());
+				config.addDataSourceProperty("prepStmtCacheSqlLimit",
+						jHipsterProperties.getDatasource()
+								.getPrepStmtCacheSqlLimit());
+			}
 
-            config.setDataSourceClassName(dataSourceProperties.getDriverClassName());
-            config.addDataSourceProperty("url", herokuUrl);
-            return new HikariDataSource(config);
-        } else {
-            throw new ApplicationContextException("Heroku database URL is not configured, you must set $JDBC_DATABASE_URL");
-        }
-    }
+			config.setDataSourceClassName(dataSourceProperties
+					.getDriverClassName());
+			config.addDataSourceProperty("url", herokuUrl);
+			return new HikariDataSource(config);
+		} else {
+			throw new ApplicationContextException(
+					"Heroku database URL is not configured, you must set $DATABASE_URL");
+		}
+	}
 }
