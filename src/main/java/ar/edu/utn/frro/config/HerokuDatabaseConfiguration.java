7f48b2066dac1d4a2ad99a169dc92679a2c03d39
Facundo Scoccia <fanky10@gmail.com>
2016-06-15 13:05:12 -0300

    Add user / password from database Uri

--- src/main/java/ar/edu/utn/frro/config/HerokuDatabaseConfiguration.java
+++ src/main/java/ar/edu/utn/frro/config/HerokuDatabaseConfiguration.java
@@ -2,18 +2,19 @@ package ar.edu.utn.frro.config;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.ApplicationContextException;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @Profile(Constants.SPRING_PROFILE_HEROKU)
@@ -29,13 +30,20 @@ public class HerokuDatabaseConfiguration {
 
 		String herokuDatabaseUrl = System.getenv("DATABASE_URL");
 		if (herokuDatabaseUrl != null) {
-			java.net.URI databaseURI = new URI(herokuDatabaseUrl);
-			String jdbcDatabaseURL = "jdbc:postgresql://" + databaseURI.getHost() + ':' + databaseURI.getPort() + databaseURI.getPath();
-			log.info("Web application on Heroku, using jdbc database url: {}", jdbcDatabaseURL);
 			HikariConfig config = new HikariConfig();
+			java.net.URI databaseURI = new URI(herokuDatabaseUrl);
+			String jdbcDatabaseURL = "jdbc:postgresql://"
+					+ databaseURI.getHost() + ':' + databaseURI.getPort()
+					+ databaseURI.getPath();
+			String username = databaseURI.getUserInfo().split(":")[0];
+			String password = databaseURI.getUserInfo().split(":")[1];
+
 			config.setDataSourceClassName(dataSourceProperties
 					.getDriverClassName());
 			config.addDataSourceProperty("url", jdbcDatabaseURL);
+			config.addDataSourceProperty("user", username);
+			config.addDataSourceProperty("password", password);
+
 			return new HikariDataSource(config);
 		} else {
 			throw new ApplicationContextException(
