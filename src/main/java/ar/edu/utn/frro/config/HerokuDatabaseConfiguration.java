78bae3862aa798c98bcbef693f3771d7192fa990
Facundo Scoccia <fanky10@gmail.com>
2016-06-16 10:24:04 -0300

    Revert Procfile + HerokuDatabaseConfiguration

--- src/main/java/ar/edu/utn/frro/config/HerokuDatabaseConfiguration.java
+++ src/main/java/ar/edu/utn/frro/config/HerokuDatabaseConfiguration.java
@@ -1,41 +1,48 @@
 package ar.edu.utn.frro.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.ApplicationContextException;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-import javax.sql.DataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @Profile(Constants.SPRING_PROFILE_HEROKU)
 public class HerokuDatabaseConfiguration {
 
-    private final Logger log = LoggerFactory.getLogger(HerokuDatabaseConfiguration.class);
+	private final Logger log = LoggerFactory
+			.getLogger(HerokuDatabaseConfiguration.class);
 
-    @Bean
-    public DataSource dataSource(DataSourceProperties dataSourceProperties, JHipsterProperties jHipsterProperties) {
-        log.debug("Configuring Heroku Datasource");
+	@Bean
+	public DataSource dataSource(DataSourceProperties dataSourceProperties,
+			JHipsterProperties jHipsterProperties) throws URISyntaxException {
+		String jdbcDatabaseUrl = System.getenv("JDBC_DATABASE_URL");
 
-        String herokuUrl = System.getenv("JDBC_DATABASE_URL");
-        if (herokuUrl != null) {
-	    HikariConfig config = new HikariConfig();
+		if (jdbcDatabaseUrl != null) {
+			HikariConfig config = new HikariConfig();
+			String username = System.getenv("JDBC_DATABASE_USERNAME");
+			String password = System.getenv("JDBC_DATABASE_PASSWORD");
 
-	    //MySQL optimizations, see https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
-	    if ("com.mysql.jdbc.jdbc2.optional.MysqlDataSource".equals(dataSourceProperties.getDriverClassName())) {
-                config.addDataSourceProperty("cachePrepStmts", jHipsterProperties.getDatasource().isCachePrepStmts());
-                config.addDataSourceProperty("prepStmtCacheSize", jHipsterProperties.getDatasource().getPrepStmtCacheSize());
-                config.addDataSourceProperty("prepStmtCacheSqlLimit", jHipsterProperties.getDatasource().getPrepStmtCacheSqlLimit());
-            }
+			config.setDataSourceClassName(dataSourceProperties
+					.getDriverClassName());
+			config.addDataSourceProperty("url", jdbcDatabaseUrl);
+			config.addDataSourceProperty("user", username);
+			config.addDataSourceProperty("password", password);
 
-            config.setDataSourceClassName(dataSourceProperties.getDriverClassName());
-            config.addDataSourceProperty("url", herokuUrl);
-            return new HikariDataSource(config);
-        } else {
-            throw new ApplicationContextException("Heroku database URL is not configured, you must set $JDBC_DATABASE_URL");
-        }
-    }
+			return new HikariDataSource(config);
+		} else {
+			throw new ApplicationContextException(
+					"Heroku database URL is not configured, you must set $DATABASE_URL");
+		}
+	}
 }
