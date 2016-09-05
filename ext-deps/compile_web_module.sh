40ac40095a1a23a6a6c030617483a53de9c18d4d
Facundo Scoccia <fanky10@gmail.com>
2016-09-05 16:10:26 -0300

    Change web deploy script

--- ext-deps/compile_web_module.sh
+++ ext-deps/compile_web_module.sh
@@ -2,14 +2,5 @@
 echo "Updating web module"
 git submodule update --init --recursive
 
-echo "Initializing"
-cd web-module
-npm cache clean -f
-npm install
-
-echo "Compiling"
-gulp
-
 echo "Distributing"
-rm -r ../src/main/webapp/dist
-mv dist ../src/main/webapp
+mv web-module/* src/main/webapp/
